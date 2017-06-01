bae6c247bea2a32ccfb16c0448a3079f1baf44af
SimoneRicci98 <ricci.simone98@gmail.com>
2017-06-01 09:02:39 +0200

    Added a section for number of customers ecc

--- progetto esame/progetto/Seleziona.aspx.cs
+++ progetto esame/progetto/Seleziona.aspx.cs
@@ -12,6 +12,9 @@ public partial class Fatturazione : System.Web.UI.Page
      SqlDataReader rs;
     protected void Page_Load(object sender, EventArgs e)
     {
+        string numFornitori;
+        string numClienti;
+        string numFatture;
         help.connetti();
         help.assegnaComando("SELECT Versione FROM Utenti WHERE ID_Utente = " + Session["Utente"].ToString());
         rs = help.estraiDati();
@@ -31,23 +34,43 @@ public partial class Fatturazione : System.Web.UI.Page
         help.disconnetti();
         #region conto clienti, fornitori e fatture create
         help.connetti();
-        help.assegnaComando("SELECT COUNT(COD_Azienda) FROM Clienti WHERE COD_Azienda = " + Session["Azienda"].ToString());
+        help.assegnaComando("SELECT COUNT(COD_Azienda) FROM Clienti WHERE COD_Azienda = '" + Session["Azienda"].ToString()+"'");
         rs = help.estraiDati();
-        string numClienti = rs[0].ToString();
+        rs.Read();
+        numClienti = rs[0].ToString();
         help.disconnetti();
         help.connetti();
-        help.assegnaComando("SELECT COUNT(COD_Azienda) FROM Fornitori WHERE COD_Azienda = " + Session["Azienda"].ToString());
+        help.assegnaComando("SELECT COUNT(COD_Azienda) FROM Fornitori WHERE COD_Azienda = '" + Session["Azienda"].ToString()+"'");
         rs = help.estraiDati();
-        string numFornitori = rs[0].ToString();
+        rs.Read();
+        numFornitori = rs[0].ToString();
         help.disconnetti();
         help.connetti();
-        help.assegnaComando("CREATE VIEW vista1 as(SELECT DISTINCT COUNT(ID_Fattura) as conta FROM Fattura WHERE COD_Azienda ="+Session["Azienda"].ToString()+")");
+        help.assegnaComando("CREATE VIEW vista1 as(SELECT DISTINCT COUNT(ID_Fattura) as conta FROM Fattura WHERE COD_Azienda ='"+Session["Azienda"].ToString()+"')");
         help.eseguicomando();
         help.disconnetti();
         help.connetti();
-        help.assegnaComando("SELECT DISTINCT COUNT(Conta) FROM vista1");
+        help.assegnaComando("SELECT conta FROM vista1");
         rs = help.estraiDati();
-        string numFatture = rs[0].ToString();
+        rs.Read();
+        if (rs["conta"].ToString() == "0")
+        {
+            numFatture = rs["conta"].ToString();
+            help.disconnetti();
+        }
+        else
+        {
+            help.disconnetti();
+            help.connetti();
+            help.assegnaComando("SELECT DISTINCT COUNT(Conta) FROM vista1");
+            rs = help.estraiDati();
+            rs.Read();
+            numFatture = rs[0].ToString();
+            help.disconnetti();
+        }
+        help.connetti();
+        help.assegnaComando("DROP VIEW Vista1");
+        help.eseguicomando();
         help.disconnetti();
         #endregion
         lblLimitazioni.Text = "Hai "+numClienti+" clienti, "+numFornitori+" fornitori e hai creato "+numFatture+" fatture finora.";
